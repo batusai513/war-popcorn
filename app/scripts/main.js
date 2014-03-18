164a4ab6b03b3b2bf9a3778a78ef871183d99d84
Richard Roncancio <batusai513@msn.com>
2014-03-18 00:35:34 -0500

    more

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -44,6 +44,7 @@ var MoviePresenter = function(_view){
 		model;
 
 	function getView(){
+		console.log(view)
 		return view.getView();
 	}
 
@@ -101,8 +102,6 @@ var CollectionPresenter = function(_view, _collection){
 	function createItem(model){
 		var movie = new MoviePresenter(new MovieView({tmp: "#movie-item"}));
 		movie.setModel(model);
-		console.log(model);
-		console.log(movie.getView())
 		$(view).append(movie.getView())
 	}
 
@@ -114,12 +113,6 @@ var CollectionPresenter = function(_view, _collection){
 		init: init
 	}
 }
-
-var movie = new Movie({"adult":false,"backdrop_path":"/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg","belongs_to_collection":null,"budget":63000000,"genres":[{"id":28,"name":"Action"},{"id":18,"name":"Drama"},{"id":53,"name":"Thriller"}],"homepage":"","id":550,"imdb_id":"tt0137523","original_title":"Fight Club","overview":"A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.","popularity":9.34757776181267,"poster_path":"/2lECpi35Hnbpa4y46JX0aY3AWTy.jpg","production_companies":[{"name":"20th Century Fox","id":25},{"name":"Fox 2000 Pictures","id":711},{"name":"Regency Enterprises","id":508}],"production_countries":[{"iso_3166_1":"DE","name":"Germany"},{"iso_3166_1":"US","name":"United States of America"}],"release_date":"1999-10-14","revenue":100853753,"runtime":139,"spoken_languages":[{"iso_639_1":"en","name":"English"}],"status":"Released","tagline":"How much can you know about yourself if you've never been in a fight?","title":"Fight Club","vote_average":7.6,"vote_count":2825})
-var presenter = new MoviePresenter(new MovieView({el: "div", tmp: "#movie-item"}))
-presenter.setModel(movie)
-var p = presenter.getView();
-
 
 
 var App = App || {};
@@ -151,6 +144,7 @@ function request(url){
 		}
 	)).
 		done(function(data){
+			console.log(data)
 			deferred.resolve(data);
 	}).
 		fail(function(){
@@ -160,9 +154,17 @@ function request(url){
 	return deferred.promise();
 }
 
-var c = new Collection({
-url: 'movie/popular'
+var c1 = new Collection({
+	url: 'movie/popular',
+	type: 'popular'
 })
-var cp = new CollectionPresenter("#popupar", c)
-cp.init()
+var popular = new CollectionPresenter("#popular", c1)
+popular.init()
+
+var c2 = new Collection({
+	url: 'movie/upcoming',
+	type: 'popular'
+})
+var latest = new CollectionPresenter("#latest", c2)
+latest.init()
 
